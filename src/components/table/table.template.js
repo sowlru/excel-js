76c3b1d8a17f0e99b23319e6a358a66e2849c4e1
Alex VK <aklimovskikh@yandex.ru>
2022-07-16 22:42:58 +0300

    function create-table was created

---
+++ src/components/table/table.template.js
@@ -0,0 +1,28 @@
+const CODES = {A: 65, Z: 90}
+
+// function createCell() {
+//   return `<div class='cell' contenteditable>B2</div>`
+// }
+
+function toChar(_, idx) {
+  return String.fromCharCode(CODES.A + idx)
+}
+function toCol(col) {
+  return `<div class='column'>${col}</div>`
+}
+function createRow(content) {
+  return `
+  <div class='row'>
+    <div class='row-info'></div>
+    <div class='row-data'>${content}</div>
+  </div>`
+}
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1
+  const rows = []
+  const cols = new Array(colsCount).fill('').map(toChar).map(toCol).join('')
+
+  rows.push(createRow(cols))
+  for (let i=0; i < rowsCount; i++) rows.push(createRow())
+  return rows.join('')
+}
