f2f60fae136786c6d9c01571f1af338a8e70d860
Alex VK <aklimovskikh@yandex.ru>
2022-08-23 20:53:48 +0300

    create createStore.js

---
+++ src/core/createStore.js
@@ -0,0 +1,21 @@
+export function createStore(rootReducer, initiialState = {}) {
+  let state = rootReducer({...initiialState}, {type: '__INIT__'})
+  let listeners = []
+  return {
+    subscribe(fn) {
+      listeners.push(fn)
+      return {
+        unsubscribe() {
+          listeners = listeners.filter((l) => l !== fn)
+        },
+      }
+    },
+    dispatch(action) {
+      state = rootReducer(state, action)
+      listeners.forEach((l) => l(state))
+    },
+    getState() {
+      return state
+    },
+  }
+}
