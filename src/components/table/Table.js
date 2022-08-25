00881c7131ba4141204f9a71bfc93b226a0e1cdb
Alex VK <aklimovskikh@yandex.ru>
2022-08-25 20:53:49 +0300

    create column state

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -41,9 +41,18 @@ export class Table extends ExcelComponent {
     this.selection.select($cell)
     this.$emit('table:select', $cell)
   }
+  async resizeTable(event) {
+    try {
+      const data = await resizeHandler(this.$root, event)
+      this.$dispatch({type: 'TABLE_RESIZE', data})
+      console.log('Resize data', data)
+    } catch (e) {
+      console.warn('Resize error', e.message)
+    }
+  }
   onMousedown(event) {
     if (shouldResize(event)) {
-      resizeHandler(this.$root, event)
+      this.resizeTable(event)
     } else if (isCell(event)) {
       const $target = $(event.target)
       if (event.shiftKey) {
