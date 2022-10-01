59577c7ac85b69f31c3daa0a04d30a0e61233751
Alex VK <aklimovskikh@yandex.ru>
2022-10-01 21:01:01 +0300

    addd jest settings

---
+++ src/core/createStore.spec.js
@@ -0,0 +1,10 @@
+import {createStore} from './createStore'
+
+describe('TEST', () => {
+
+  test('test' , () => {
+    const store = createStore(() => {}, {})
+    expect(store).toBeDefined()
+  })
+
+})
