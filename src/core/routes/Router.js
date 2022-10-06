ba9d187c051c79f667a0f2bf04334023f8667210
Alex <aklimovskikh@yandex.ru>
2022-10-06 21:28:36 +0300

    implement Dependency Inversion (#9)

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,3 +1,4 @@
+import {Loader} from '../../components/Loader'
 import {$} from '../dom'
 import {ActiveRoute} from './ActiveRoute'
 
@@ -9,6 +10,7 @@ export class Router {
     this.$placeholder = $(selector)
     this.routes = routes
 
+    this.loader = new Loader()
     this.page = null
 
     this.changePageHandler = this.changePageHandler.bind(this)
@@ -18,16 +20,17 @@ export class Router {
     window.addEventListener('hashchange', this.changePageHandler)
     this.changePageHandler()
   }
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
       this.page.destroy()
     }
-    this.$placeholder.clear()
+    this.$placeholder.clear().append(this.loader)
     const Page = ActiveRoute.path.includes('excel') ?
       this.routes.excel :
       this.routes.dashboard
     this.page = new Page(ActiveRoute.param)
-    this.$placeholder.append(this.page.getRoot())
+    const root = await this.page.getRoot()
+    this.$placeholder.clear().append(root)
     this.page.afterRender()
   }
   destroy() {
--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -13,15 +13,52 @@ function storageName(param) {
   return 'excel:' + param
 }
 
+class StateProcessor {
+  constructor(client, delay = 300) {
+    this.client = client
+    this.listen = debounce(this.listen.bind(this), delay)
+  }
+  listen(state) {
+    this.client.save(state)
+  }
+  get() {
+    return this.client.get()
+  }
+}
+
+class LocalStorageClient {
+  constructor(name) {
+    this.name = storageName(name)
+  }
+  save(state) {
+    storage(this.name, state)
+    return Promise.resolve()
+  }
+
+  get() {
+    // return Promise.resolve(storage(this.name))
+    return new Promise((resolve) => {
+      const state = storage(this.name)
+      setTimeout(() => {
+        resolve(state)
+      }, 3000)
+    })
+  }
+}
 export class ExcelPage extends Page {
-  getRoot() {
-    const params = this.params ? this.params : Date.now().toString()
-    const state = storage(storageName(params))
-    const store = createStore(rootReducer, normalizeInitialState(state))
-    const stateListener = debounce((state) => {
-      storage(storageName(params), state)
-    }, 500)
-    store.subscribe(stateListener)
+  constructor(param) {
+    super(param)
+    this.storeSub = null
+    this.processor = new StateProcessor(
+        new LocalStorageClient(this.params)
+    )
+  }
+  async getRoot() {
+    const state = await this.processor.get()
+    const initialState = normalizeInitialState(state)
+    const store = createStore(rootReducer, initialState)
+
+    this.storeSub = store.subscribe(this.processor.listen)
     this.excel = new Excel({
       components: [Header, Toolbar, Formula, Table],
       store,
@@ -33,5 +70,6 @@ export class ExcelPage extends Page {
   }
   destroy() {
     this.excel.destroy()
+    this.storeSub.unsubscribe()
   }
 }
